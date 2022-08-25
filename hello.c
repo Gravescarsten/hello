5076a7f44833040acb89379130394246352cbd41
Gravescarsten <cgraves6@huskers.unl.edu>
2022-08-25 13:46:40 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Carsten
+ * Date: 08/25/2022
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+
+  printf("Carsten Graves\n");
+
+
+  return 0;
+}
