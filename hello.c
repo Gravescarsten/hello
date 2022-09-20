e48eb2d7f6aeb33b0c1f18152e3228843fb86e1a
Djenkins412 <djenkins8@huskers.unl.edu>
2022-09-20 15:18:47 +0000

    Added partner credentials

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Carsten
+ * Author: Carsten Graves, Dezmond Jenkins
  * Date: 08/25/2022
  *
  */
@@ -11,6 +11,8 @@ int main(int argc, char **argv) {
 
   printf("Carsten Graves!\n");
   printf("Computer Engineering!\n");
+  printf ("Hello World, I'm Dezmond Jenkins\n");
+  printf("I'm a Computer Engineering Major\n");
 
 
   return 0;
